875f7dc54603b0218227c7c9fb4da747f885bd80
vvengers <v.p.g.vanengers@student.utwente.nl>
2023-01-30 12:20:13 +0100

    Added parameters for boolean order

--- utils.py
+++ utils.py
@@ -7,8 +7,8 @@ from pathlib import Path
 
 def save(obj, path, name, overwrite=True):
     Path(path).mkdir(parents=True, exist_ok=True)
-    if not(not overwrite & os.path.isfile(path+name)):
+    if not((not overwrite) & os.path.isfile(path+name)):
         torch.save(obj, path + name)
     else:
         # add time stamp to name if file is already there, and it shouldn't be overwritten
-        torch.save(obj, path + name + time.time())
+        torch.save(obj, path + name + str(time.time()))
