e4b1f1662dcfecabd29e221c665bdafcd6aef986
vvengers <v.p.g.vanengers@student.utwente.nl>
2023-02-01 14:00:04 +0100

    Transforms from paper

--- dataloader.py
+++ dataloader.py
@@ -11,10 +11,10 @@ def load_data(root_dir='./data/Incidents-subset', val_size=0.05, test_size=0.05,
     # Define the transforms to apply to your images
     # In this example, we resize the images to 256x256 and normalize them
     transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(256),
+        transforms.Resize((256, 256)),
+        transforms.CenterCrop(224),
         transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
     # Use the ImageFolder class to load the images from your root directory
