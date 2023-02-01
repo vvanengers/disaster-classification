db5adf2e1bcc593e02ba34a9ef6dd727fcc1ded7
vvengers <v.p.g.vanengers@student.utwente.nl>
2023-02-01 12:00:15 +0100

    Only validate at the end of training model

--- main.py
+++ main.py
@@ -74,64 +74,19 @@ def train_model(args, device, model, criterion, optimizer, scheduler, dataloader
 
         # Each epoch has a training and validation phase
         # for phase in ['train', 'val']:
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                model.train()  # Set model to training mode
-            else:
-                model.eval()  # Set model to evaluate mode
+        best_acc, best_model_wts, optimizer_state_dict, state_dict = train_epoch('train', args, best_acc, best_model_wts,
+                                                                                 criterion, dataloaders, device, epoch,
+                                                                                 loss_hist, loss_list, mask, model,
+                                                                                 model_save_path, optimizer,
+                                                                                 result_save_path, save_name, scheduler,
+                                                                                 train_hist, val_hist)
 
-            running_loss = 0.0
-            running_corrects = 0
-
-            # Iterate over data.
-            for i, (inputs, labels) in enumerate(dataloaders[phase]):
-                # print(f'{i}/{len(dataloaders[phase])}')
-
-                inputs = inputs.to(device)
-                labels = labels.to(device)
-
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs = model(inputs)
-                    _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
-
-                    # backward + optimize only if in training phase
-                    if phase == 'train':
-                        loss.backward()
-                        if mask is not None:
-                            mask.step()
-                        else:
-                            optimizer.step()
-
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-            if phase == 'train':
-                scheduler.step()
-
-            epoch_loss = running_loss / len(dataloaders[phase])
-            epoch_acc = running_corrects.double() / len(dataloaders[phase])
-            loss_list.append(epoch_loss)
-            (train_hist if phase == 'train' else val_hist).append(epoch_acc)
-
-            print_and_log(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
-
-            # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = copy.deepcopy(model.state_dict())
-
-        state_dict = model.state_dict()
-        optimizer_state_dict = optimizer.state_dict()
-        checkpoint(train_hist, val_hist, loss_hist, args, best_acc, epoch, model_save_path, optimizer_state_dict,
-                   result_save_path,
-                   save_name, state_dict)
-
+    best_acc, best_model_wts, optimizer_state_dict, state_dict = train_epoch('val', args, best_acc, best_model_wts,
+                                                                             criterion, dataloaders, device, epoch,
+                                                                             loss_hist, loss_list, mask, model,
+                                                                             model_save_path, optimizer,
+                                                                             result_save_path, save_name, scheduler,
+                                                                             train_hist, val_hist)
     time_elapsed = time.time() - since
     print_and_log(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
     print_and_log(f'Best val Acc: {best_acc:4f}')
@@ -144,6 +99,61 @@ def train_model(args, device, model, criterion, optimizer, scheduler, dataloader
                result_save_path,
                save_name, state_dict)
     return model
+
+
+def train_epoch(phase, args, best_acc, best_model_wts, criterion, dataloaders, device, epoch, loss_hist, loss_list, mask,
+                model, model_save_path, optimizer, result_save_path, save_name, scheduler, train_hist, val_hist):
+    if phase == 'train':
+        model.train()  # Set model to training mode
+    else:
+        model.eval()  # Set model to evaluate mode
+    running_loss = 0.0
+    running_corrects = 0
+    # Iterate over data.
+    for i, (inputs, labels) in enumerate(dataloaders[phase]):
+        # print(f'{i}/{len(dataloaders[phase])}')
+
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward
+        # track history if only in train
+        with torch.set_grad_enabled(phase == 'train'):
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            loss = criterion(outputs, labels)
+
+            # backward + optimize only if in training phase
+            if phase == 'train':
+                loss.backward()
+                if mask is not None:
+                    mask.step()
+                else:
+                    optimizer.step()
+
+        # statistics
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data)
+    if phase == 'train':
+        scheduler.step()
+    epoch_loss = running_loss / len(dataloaders[phase])
+    epoch_acc = running_corrects.double() / len(dataloaders[phase])
+    loss_list.append(epoch_loss)
+    (train_hist if phase == 'train' else val_hist).append(epoch_acc)
+    print_and_log(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+    # deep copy the model
+    if phase == 'val' and epoch_acc > best_acc:
+        best_acc = epoch_acc
+        best_model_wts = copy.deepcopy(model.state_dict())
+    state_dict = model.state_dict()
+    optimizer_state_dict = optimizer.state_dict()
+    checkpoint(train_hist, val_hist, loss_hist, args, best_acc, epoch, model_save_path, optimizer_state_dict,
+               result_save_path,
+               save_name, state_dict)
+    return best_acc, best_model_wts, optimizer_state_dict, state_dict
 
 
 def checkpoint(train_hist, val_hist, loss_hist, args, best_acc, epoch, model_save_path, optimizer_state_dict, result_save_path,
